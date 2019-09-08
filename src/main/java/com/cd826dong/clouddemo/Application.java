1daeb918d2cb65c83cf513d830ffa4e1b871e6bf
kouyt5 <30313488+kouyt5@users.noreply.github.com>
2019-09-08 18:28:16 +0800

    init

--- src/main/java/com/cd826dong/clouddemo/Application.java
+++ src/main/java/com/cd826dong/clouddemo/Application.java
@@ -18,7 +18,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-
 /**
  * 应用主应用类
  *
